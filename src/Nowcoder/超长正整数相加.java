b92ca336d0508b4e7c66869d8bbacdbb76fad0c2
hao-yi12138 <692180261@qq.com>
2020-12-08 16:59:10 +0800

    超长正整数相加

---
+++ src/Nowcoder/超长正整数相加.java
@@ -0,0 +1,23 @@
+package Nowcoder;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * @program: IntelliJ IDEA
+ * @description:
+ * @author: HAOYI
+ * @date:2020-12-08 11:26
+ **/
+public class 超长正整数相加 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        while(input.hasNext()){
+            String s1=input.next();
+            String s2=input.next();
+            BigInteger num1=new BigInteger(s1);//采用BigInteger可以直接进行大整数进行计算
+            BigInteger num2=new BigInteger(s2);
+            System.out.println(num1.add(num2));
+        }
+    }
+}
